f5391e3dad10b8f21e7bb847b8fd7a4cd053b23d
meooow25 <soumiksarkar.3120@gmail.com>
2024-05-04 09:24:44 +0530

    More benchmarks
    * Benchmark sorting unboxed vectors
    * Include vector-algorithms heapsort

--- compare/Main.hs
+++ compare/Main.hs
@@ -1,9 +1,11 @@
+{-# OPTIONS_GHC -Wno-orphans #-} -- SPECIALIZE rules
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE MagicHash #-}
+{-# LANGUAGE RankNTypes #-}
 
 import Control.DeepSeq (NFData(..), rwhnf)
 import Control.Monad.Primitive (PrimMonad(..), RealWorld, stToPrim)
-import Control.Monad.ST (stToIO)
+import Control.Monad.ST (ST, stToIO)
 import Data.Bits ((.&.))
 import Data.Primitive.Array
   ( MutableArray(..)
@@ -11,9 +13,10 @@ import Data.Primitive.Array
   , sizeofMutableArray
   , writeArray
   )
-import Data.Primitive.ByteArray (MutableByteArray(..))
+import Data.Primitive.ByteArray (ByteArray(..), MutableByteArray(..))
 import Data.Primitive.PrimArray
   ( MutablePrimArray(..)
+  , PrimArray(..)
   , getSizeofMutablePrimArray
   , newPrimArray
   , writePrimArray
@@ -21,13 +24,21 @@ import Data.Primitive.PrimArray
 import Data.Primitive.Types (Prim)
 import qualified Data.List as L
 import qualified Data.Primitive.Sort as PSort
+import qualified Data.Vector.Algorithms.Heap as Heap
 import qualified Data.Vector.Algorithms.Intro as Intro
 import qualified Data.Vector.Algorithms.Merge as Merge
 import qualified Data.Vector.Algorithms.Tim as Tim
 import qualified Data.Vector.Primitive.Mutable as MPV
 import qualified Data.Vector.Mutable as MV
+import qualified Data.Vector.Primitive as VP
+import qualified Data.Vector.Primitive.Mutable as VPM
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Base as VUB
+import qualified Data.Vector.Unboxed.Mutable as VUM
+import qualified Data.Vector.Generic as VG
+import Data.Word (Word8)
 
-import Criterion.Main (Benchmark, defaultMain, bench, bgroup, perRunEnv)
+import Criterion.Main (Benchmark, defaultMain, bench, bgroup, perRunEnv, whnf)
 
 import qualified Data.SamSort as Sam
 
@@ -36,6 +47,8 @@ main = defaultMain
   [ bgroup "Int" $ map (bgroupN id) sizes
   , bgroup "Maybe Int" $ map (bgroupN Just) sizes
   , bgroup "Int (Prim)" $ map bgroupPN sizes
+  , bgroup "Int*3 (Unboxed)" $ map bgroupVUN sizes
+  , bgroup "Word8 (Unboxed)" $ map bgroupW8VUN sizes
   ]
 
 sizes :: [Int]
@@ -61,11 +74,13 @@ bgroupN f n = bgroup (show n) $
 bgroupIOA :: Ord a => String -> IO (IOArray a) -> Benchmark
 bgroupIOA name mkma = bgroup name
   [ bench "samsort sortArrayBy" $
-    perRunEnv (fmap WHNF mkma) $ \(WHNF ma) -> samSort ma
-  , bench "vector-algorithms Intro" $
-    perRunEnv mkmv $ \(WHNF mv) -> Intro.sort mv
+    perRunEnv (fmap WHNF mkma) $ \(WHNF ma) -> samSortArray ma
   , bench "vector-algorithms Merge" $
     perRunEnv mkmv $ \(WHNF mv) -> Merge.sort mv
+  , bench "vector-algorithms Intro" $
+    perRunEnv mkmv $ \(WHNF mv) -> Intro.sort mv
+  , bench "vector-algorithms Heap" $
+    perRunEnv mkmv $ \(WHNF mv) -> Heap.sort mv
   , bench "vector-algorithms Tim" $
     perRunEnv mkmv $ \(WHNF mv) -> Tim.sort mv
   ]
@@ -92,11 +107,13 @@ bgroupPN n = bgroup (show n) $
 bgroupIOPA :: String -> IO (IOPrimArray Int) -> Benchmark
 bgroupIOPA name mkma = bgroup name
   [ bench "samsort sortIntArrayBy" $
-    perRunEnv (fmap WHNF mkma) $ \(WHNF ma) -> samSortInts ma
-  , bench "vector-algorithms Intro" $
-    perRunEnv mkmv $ \(WHNF mv) -> Intro.sort mv
+    perRunEnv (fmap WHNF mkma) $ \(WHNF ma) -> samSortIntArray ma
   , bench "vector-algorithms Merge" $
     perRunEnv mkmv $ \(WHNF mv) -> Merge.sort mv
+  , bench "vector-algorithms Intro" $
+    perRunEnv mkmv $ \(WHNF mv) -> Intro.sort mv
+  , bench "vector-algorithms Heap" $
+    perRunEnv mkmv $ \(WHNF mv) -> Heap.sort mv
   , bench "vector-algorithms Tim" $
     perRunEnv mkmv $ \(WHNF mv) -> Tim.sort mv
   , bench "primitive-sort" $
@@ -109,14 +126,132 @@ bgroupIOPA name mkma = bgroup name
       sz <- getSizeofMutablePrimArray ma
       pure (WHNF (MPV.MVector 0 sz (MutableByteArray ma#)))
 
-samSort :: (PrimMonad m, Ord a) => MutableArray (PrimState m) a -> m ()
-samSort ma@(MutableArray ma#) =
+bgroupVUN :: Int -> Benchmark
+bgroupVUN n = bgroup (show n) $
+  if n <= 1
+  then
+    [ bgroupVU "Same" (sameVU n)
+    ]
+  else
+    [ bgroupVU "Asc" (ascVU n)
+    , bgroupVU "Desc" (descVU n)
+    , bgroupVU "UpDownAsc" (upDownAscVU n)
+    , bgroupVU "UpDownDesc" (upDownDescVU n)
+    , bgroupVU "Rand" (randVU n)
+    , bgroupVU "RandSmall" (randSmallVU n)
+    , bgroupVU "AscThenRand" (ascThenRandVU n)
+    , bgroupVU "RandThenAsc" (randThenAscVU n)
+    ]
+
+bgroupVU :: String -> VU.Vector (Int, Int, Int) -> Benchmark
+bgroupVU name !v = bgroup name
+  [ bench "samsort sortIntArrayBy (by index)" $
+    whnf samSortVecByIdx v
+  , bench "vector-algorithms Merge" $
+    whnf (sortVecUsing Merge.sort) v
+  , bench "vector-algorithms Intro" $
+    whnf (sortVecUsing Intro.sort) v
+  , bench "vector-algorithms Heap" $
+    whnf (sortVecUsing Heap.sort) v
+  , bench "vector-algorithms Tim" $
+    whnf (sortVecUsing Tim.sort) v
+  , bench "vector-algorithms Merge (by index)" $
+    whnf (sortVecByIdxUsing Merge.sortBy) v
+  , bench "vector-algorithms Intro (by index)" $
+    whnf (sortVecByIdxUsing Intro.sortBy) v
+  , bench "vector-algorithms Heap (by index)" $
+    whnf (sortVecByIdxUsing Heap.sortBy) v
+  , bench "vector-algorithms Tim (by index)" $
+    whnf (sortVecByIdxUsing Tim.sortBy) v
+  ]
+
+bgroupW8VUN :: Int -> Benchmark
+bgroupW8VUN n = bgroup (show n) $
+  if n <= 1
+  then
+    [ bgroupW8VU "Same" (sameW8VU n)
+    ]
+  else
+    [ bgroupW8VU "RandSmall" (randSmallW8VU n)
+    ]
+
+bgroupW8VU :: String -> VU.Vector Word8 -> Benchmark
+bgroupW8VU name !v = bgroup name
+  [ bench "samsort sortIntArrayBy (by index)" $
+    whnf samSortVecByIdx v
+  , bench "vector-algorithms Merge" $
+    whnf (sortVecUsing Merge.sort) v
+  , bench "vector-algorithms Intro" $
+    whnf (sortVecUsing Intro.sort) v
+  , bench "vector-algorithms Heap" $
+    whnf (sortVecUsing Heap.sort) v
+  , bench "vector-algorithms Tim" $
+    whnf (sortVecUsing Tim.sort) v
+  , bench "vector-algorithms Merge (by index)" $
+    whnf (sortVecByIdxUsing Merge.sortBy) v
+  , bench "vector-algorithms Intro (by index)" $
+    whnf (sortVecByIdxUsing Intro.sortBy) v
+  , bench "vector-algorithms Heap (by index)" $
+    whnf (sortVecByIdxUsing Heap.sortBy) v
+  , bench "vector-algorithms Tim (by index)" $
+    whnf (sortVecByIdxUsing Tim.sortBy) v
+  , bench "primitive-sort" $
+    whnf PSort.sort pv
+  ]
+  where
+    !(VUB.V_Word8 (VP.Vector _off _len (ByteArray ba#))) = v
+    -- Note: primitive-sort does not support sorting a slice! In this case we
+    -- know that the vector is not a slice, so we ignore off and len.
+    pv = PrimArray ba# :: PrimArray Word8
+
+---------------
+-- Sort utils
+---------------
+
+samSortArray :: (PrimMonad m, Ord a) => MutableArray (PrimState m) a -> m ()
+samSortArray ma@(MutableArray ma#) =
   stToPrim $ Sam.sortArrayBy compare ma# 0 (sizeofMutableArray ma)
 
-samSortInts :: PrimMonad m => MutablePrimArray (PrimState m) Int -> m ()
-samSortInts ma@(MutablePrimArray ma#) = do
+samSortIntArray :: PrimMonad m => MutablePrimArray (PrimState m) Int -> m ()
+samSortIntArray ma@(MutablePrimArray ma#) = do
   sz <- getSizeofMutablePrimArray ma
   stToPrim $ Sam.sortIntArrayBy compare ma# 0 sz
+
+samSortVecByIdx :: (Ord a, VG.Vector v a) => v a -> v a
+samSortVecByIdx v = VG.generate n (VG.unsafeIndex v . VP.unsafeIndex ixa)
+  where
+    n = VG.length v
+    ixa = VP.create $ do
+      ixma <- VPM.generate n id
+      let !(VPM.MVector off len (MutableByteArray ma#)) = ixma
+          cmp i j = compare (VG.unsafeIndex v i) (VG.unsafeIndex v j)
+      stToPrim $ Sam.sortIntArrayBy cmp ma# off len
+      pure ixma
+
+sortVecUsing
+  :: (Ord a, VG.Vector v a)
+  => (forall s. VG.Mutable v s a -> ST s ())
+  -> v a
+  -> v a
+sortVecUsing sortf v = VG.create $ do
+  vm <- VG.thaw v
+  sortf vm
+  pure vm
+
+sortVecByIdxUsing
+  :: (Ord a, VG.Vector v a)
+  => (forall s. (Int -> Int -> Ordering) -> VU.MVector s Int -> ST s ())
+  -> v a
+  -> v a
+sortVecByIdxUsing sortf v =
+  VG.generate n (VG.unsafeIndex v . VU.unsafeIndex ixa)
+  where
+    n = VG.length v
+    ixa = VU.create $ do
+      ixma <- VUM.generate n id
+      let cmp i j = compare (VG.unsafeIndex v i) (VG.unsafeIndex v j)
+      sortf cmp ixma
+      pure ixma
 
 ---------
 -- Data
@@ -168,6 +303,26 @@ randSmallIOPA n = mutPrimArrayFromListN n (randSmall n)
 ascThenRandIOPA n = mutPrimArrayFromListN n (ascThenRand n)
 randThenAscIOPA n = mutPrimArrayFromListN n (randThenAsc n)
 
+sameVU, ascVU, descVU, upDownAscVU, upDownDescVU,
+  randVU, randSmallVU, ascThenRandVU, randThenAscVU
+    :: Int -> VU.Vector (Int, Int, Int)
+sameVU n = VU.fromList (map i3FromInt (same n))
+ascVU n = VU.fromList (map i3FromInt (asc n))
+descVU n = VU.fromList (map i3FromInt (desc n))
+upDownAscVU n = VU.fromList (map i3FromInt (upDownAsc n))
+upDownDescVU n = VU.fromList (map i3FromInt (upDownDesc n))
+randVU n = VU.fromList (map i3FromInt (rand n))
+randSmallVU n = VU.fromList (map i3FromInt (randSmall n))
+ascThenRandVU n = VU.fromList (map i3FromInt (ascThenRand n))
+randThenAscVU n = VU.fromList (map i3FromInt (randThenAsc n))
+
+sameW8VU, randSmallW8VU :: Int -> VU.Vector Word8
+sameW8VU n = VU.fromList (map fromIntegral (same n))
+randSmallW8VU n = VU.fromList (map fromIntegral (randSmall n))
+
+i3FromInt :: Int -> (Int, Int, Int)
+i3FromInt x = let x' = fromIntegral x in (x',x',x')
+
 ----------
 -- Utils
 ----------
@@ -203,3 +358,18 @@ newtype WHNF a = WHNF a
 
 instance NFData (WHNF a) where
   rnf = rwhnf
+
+--------------------
+-- Specializations
+--------------------
+
+-- Needed because GHC fails to generate these
+-- https://gitlab.haskell.org/ghc/ghc/-/issues/24765
+{-# SPECIALIZE Heap.sort  :: VUM.MVector s (Int, Int, Int) -> ST s () #-}
+{-# SPECIALIZE Intro.sort :: VUM.MVector s (Int, Int, Int) -> ST s () #-}
+{-# SPECIALIZE Merge.sort :: VUM.MVector s (Int, Int, Int) -> ST s () #-}
+{-# SPECIALIZE Tim.sort   :: VUM.MVector s (Int, Int, Int) -> ST s () #-}
+{-# SPECIALIZE Heap.sort  :: VUM.MVector s Word8 -> ST s () #-}
+{-# SPECIALIZE Intro.sort :: VUM.MVector s Word8 -> ST s () #-}
+{-# SPECIALIZE Merge.sort :: VUM.MVector s Word8 -> ST s () #-}
+{-# SPECIALIZE Tim.sort   :: VUM.MVector s Word8 -> ST s () #-}
